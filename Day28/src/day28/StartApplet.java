70b9b2f920bbeb49534d375220751c9fc6736078
jawad <jawadusaid11@gmail.com>
2022-03-23 00:12:22 +0530

    Applet

---
+++ Day28/src/day28/StartApplet.java
@@ -0,0 +1,23 @@
+package day28;
+import java.applet.Applet;
+import java.awt.*; 
+
+
+
+public class StartApplet extends Applet
+{
+public void init() {
+	Button b = new Button();
+	add(b);
+/*public void start() {
+	
+}
+
+public void stop(){
+
+}
+*/
+
+
+}
+}
