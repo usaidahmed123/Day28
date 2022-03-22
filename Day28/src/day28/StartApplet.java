6b2f866a6f375a4974ec6a26294213fc19af94b2
jawad <jawadusaid11@gmail.com>
2022-03-23 00:15:20 +0530

    applet

--- Day28/src/day28/StartApplet.java
+++ Day28/src/day28/StartApplet.java
@@ -9,8 +9,13 @@ public class StartApplet extends Applet
 public void init() {
 	Button b = new Button();
 	add(b);
+	
+	
+	
 	setBackground(Color.black);
 	setForeground(Color.yellow);
+	
+	
 /*public void start() {
 	
 }
@@ -18,6 +23,11 @@ public void init() {
 public void stop(){
 
 }
+
+public void destroy() {
+	
+}
+
 */
 
 
